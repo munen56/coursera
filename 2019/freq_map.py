9e9dc90091b3216489d086d79a42420ffb257dda
munen56 <florian.quiec@laposte.net>
2019-04-26 22:53:28 +0200

    freq_map reconverti en fct reverse

---
+++ 2019/freq_map.py
@@ -0,0 +1,11 @@
+def reverse(Pattern):
+    print (Pattern, len(Pattern))
+    reverse=""
+    for i in range(len(Pattern)-1,-1,-1):
+        print (i)
+        reverse=reverse+Pattern[i]
+    return reverse
+
+Text="AAAACCCGGT"
+
+print (reverse(Text))
